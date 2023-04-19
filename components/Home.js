22019208b143012f93a028b7b85af761fb3e8d85
Hedi Rivas <hedi.rivas@gmail.com>
2023-04-19 11:06:45 +0200

    first commit

--- components/Home.js
+++ components/Home.js
@@ -1,6 +1,27 @@
-import React from 'react';
 import styles from '../styles/Home.module.css';
 
-export default function Home() {
-  return <div>Home</div>;
+function Home() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.background}>HOME</div>
+      <h1>Home</h1>
+    </div>
+  );
 }
+
+export default Home;
+
+/*
+
+CONSIGNES :
+
+👉  Grâce à la mécanique de Next.js et balise Link, faites en sorte que chaque item du menu affiche le composant associé.
+
+- Le composant Home sera accessible via l’URL /
+
+- Le composant Shop sera accessible via l’URL /shop
+
+- Le composant About sera accessible via l’URL /about
+
+Afin d’éviter des répétitions de code, le composant Menu est déjà chargé dans le point d’entrée de l’application, le fichier pages/_app.js.
+*/
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import Menu from '@/components/Menu';
+import Head from 'next/head';
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Navigation</title>
+      </Head>
+      <Menu />
+      <Component {...pageProps} />
+    </>
+  );
 }
